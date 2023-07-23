a7856b415e223068e53f57daa7cd553f8f32bc98
LorayneNeves <lorayne3.8neves@outlook.com>
2023-07-23 13:42:01 -0300

    update

--- page.js
+++ page.js
@@ -4,14 +4,21 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
+
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+
     const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active);
+
     if (active) {
+
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+
     } else {
+
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
     }
 }
 
@@ -28,14 +35,19 @@ form.addEventListener('submit', Button);
 
 
 function typeWriter(elemento){
+
     const textoArray = elemento.innerHTML.split('');
+
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) =>{
+
         setTimeout(() =>{
             elemento.innerHTML +=letra;
         }, 50 * i)
+        
     });
 }
+
 const titulo = document.querySelector('b');
 
 typeWriter(titulo);
--- script.js
+++ script.js
@@ -3,17 +3,49 @@ const form = document.querySelector('.page-form');
 
 const btnMobile = document.getElementById('btn-mobile');
 
+let count =1;
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+
+  nextImage();
+
+}, 5000)
+
+function nextImage(){
+
+  count++;
+    
+  if(count>4){
+        
+    count =1;
+
+  }
+  document.getElementById("radio"+count).checked = true;
+  
+}
+
 function toggleMenu(event) {
+
   if (event.type === 'touchstart') event.preventDefault();
+
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
+
   const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
+
   if (active) {
+
     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+
   } else {
+
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
   }
+  
 }
 
 btnMobile.addEventListener('click', toggleMenu);
@@ -21,23 +53,11 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 const AboutButton = (event) => {
+
   event.preventDefault();
   window.location.href = 'page.html';
+
 }
 
 form.addEventListener('submit', AboutButton);
 
-
-let count =1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function(){
-    nextImage();
-}, 5000)
-function nextImage(){
-    count++;
-    if(count>4){
-        count =1;
-    }
-    document.getElementById("radio"+count).checked = true;
-}
